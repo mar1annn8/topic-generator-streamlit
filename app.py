9577c6afea4cd185a11abca67128e49caaff7cd4
mar1annn8 <mb.gatlabayan@gmail.com>
2025-10-15 02:55:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -611,5 +611,3 @@ if not st.session_state.dataframe.empty:
                 file_name="analysis_summary.txt",
                 mime="text/plain"
             )
-
-st.markdown(f"<div style='text-align: right; font-size: 0.8em; color: grey;'>Last Updated: {last_updated_date}</div>", unsafe_allow_html=True)
